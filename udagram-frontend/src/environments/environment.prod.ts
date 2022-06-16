c870092f02ddc328f928d44ef3c23a22f0e8cd53
ovbalaban <38633081+ovbalaban@users.noreply.github.com>
2022-06-16 19:10:09 +0100

    latest version

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a79dc7b7461294fefabb6fd7f7b90155-1344142639.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
